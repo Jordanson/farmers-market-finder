26bd48dddd9b147f865ded523d16832890d73f65
Ryan Mulkowsky <ryanmulkowsky@gmail.com>
2016-09-08 13:47:36 -0400

    created query controller

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,5 @@
 // Declares the initial angular module "meanMapApp". Module grabs other controllers and services.
-var app = angular.module('meanMapApp', ['addCtrl', 'geolocation', 'gservice']);
+var app = angular.module('meanMapApp', ['addCtrl', 'queryCtrl', 'geolocation', 'gservice', 'ngRoute']);
 
 // Configures Angular routing -- showing the relevant view and controller when needed.
     .config(function($routeProvider){
@@ -9,10 +9,15 @@ var app = angular.module('meanMapApp', ['addCtrl', 'geolocation', 'gservice']);
             controller: 'addCtrl',
             templateUrl: 'partials/addForm.html',
 
-            // Find Teammates Control Panel
+        // Find Teammates Control Panel
         }).when('/find', {
             templateUrl: 'partials/queryForm.html',
 
-            // All else forward to the Join Team Control Panel
+        // Find Teammates Control Panel
+        }).when('/find', {
+            controller: 'queryCtrl',
+            templateUrl: 'partials/queryForm.html',
+
+        // All else forward to the Join Team Control Panel
         }).otherwise({redirectTo:'/join'})
     });
