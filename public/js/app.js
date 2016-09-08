39646fba6321e5c6d8f4ad2e516c8b29d3b5b8e3
Ryan Mulkowsky <ryanmulkowsky@gmail.com>
2016-09-08 13:25:27 -0400

    created partials

--- public/js/app.js
+++ public/js/app.js
@@ -1,2 +1,18 @@
 // Declares the initial angular module "meanMapApp". Module grabs other controllers and services.
 var app = angular.module('meanMapApp', ['addCtrl', 'geolocation', 'gservice']);
+
+// Configures Angular routing -- showing the relevant view and controller when needed.
+    .config(function($routeProvider){
+
+        // Join Team Control Panel
+        $routeProvider.when('/join', {
+            controller: 'addCtrl',
+            templateUrl: 'partials/addForm.html',
+
+            // Find Teammates Control Panel
+        }).when('/find', {
+            templateUrl: 'partials/queryForm.html',
+
+            // All else forward to the Join Team Control Panel
+        }).otherwise({redirectTo:'/join'})
+    });
