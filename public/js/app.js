6beca4224cee1199974842680f5d9add3076d445
Nate Reeves <nreeves1268@gmail.com>
2016-09-08 15:33:14 -0400

    updated grammar

--- public/js/app.js
+++ public/js/app.js
@@ -1,2 +1,6 @@
-// Declares the initial angular module "meanMapApp". Module grabs other controllers and services.
-var app = angular.module('meanMapApp', ['addCtrl', 'geolocation', 'gservice']);
+// app.js
+
+// Declares the initial angular module "meanMapApp". Module grabs other controllers and services. Note the use of ngRoute.
+var app = angular.module('meanMapApp', ['addCtrl', 'geolocation', 'gservice', 'ngRoute'])
+
+
